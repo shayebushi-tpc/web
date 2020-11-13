3facc84a8f2f635d6769ab57ca9eb6666acadd5d
tianpengcheng <17600718816@163>
2020-11-13 17:15:52 +0800

    swaggerui、 集成redis

--- src/main/java/com/sino/service/serviceimple/BrandServiceImpl.java
+++ src/main/java/com/sino/service/serviceimple/BrandServiceImpl.java
@@ -5,10 +5,13 @@ import com.sino.pojo.PmsBrand;
 import com.sino.pojo.PmsBrandExample;
 import com.sino.service.BrandService;
 import com.sino.service.RedisService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.lang.model.element.VariableElement;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by tianpengcheng on 11/13/2020.
@@ -20,6 +23,8 @@ public class BrandServiceImpl implements BrandService {
     private PmsBrandMapper brandMapper;
     @Autowired
     private RedisService redis;
+    private  final org.slf4j.Logger logger = LoggerFactory.getLogger(BrandService.class);
+
     @Override
     public List<PmsBrand> queryBrandList() {
         List<PmsBrand> pmsBrands = brandMapper.selectByExample(new PmsBrandExample());
@@ -30,8 +35,14 @@ public class BrandServiceImpl implements BrandService {
     public PmsBrand queryById(long id) {
         PmsBrandExample pmsBrandExample = new PmsBrandExample();
         pmsBrandExample.createCriteria().andIdEqualTo(id);
-
-        return brandMapper.selectByExample(pmsBrandExample).get(0);
+        PmsBrand pmsBrand = brandMapper.selectByExample(pmsBrandExample).get(0);
+        redis.setObj(pmsBrand.getId()+"",pmsBrand);
+        if(id == 2){
+            PmsBrand obj = (PmsBrand) redis.getObj(id + "");
+            logger.info("从缓存中取出");
+            return obj;
+        }
+        return pmsBrand;
     }
 
     @Override
--- src/test/java/com/sino/MybatisApplicationTests.java
+++ src/test/java/com/sino/MybatisApplicationTests.java
@@ -1,13 +1,26 @@
 package com.sino;
 
-import org.junit.jupiter.api.Test;
+import com.sino.service.RedisService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.security.RunAs;
 
 @SpringBootTest
 class MybatisApplicationTests {
 
+	@Autowired
+	RedisTemplate redis;
 	@Test
 	void contextLoads() {
+		redis.opsForValue().set("key","aaa");
+	}
+	@Test
+	void getKey(){
+		Object key = redis.opsForValue().get("key");
+		System.out.println(key);
 	}
 
 }
